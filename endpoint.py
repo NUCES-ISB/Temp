5fe30dfb0fd3a691b1b7713423afdbb991c9d1b3
Zaafir Rizwan <125952021+Zaafir-ctrl@users.noreply.github.com>
2023-03-18 21:13:05 +0500

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify,render_template
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pandas as pd
 import os
 from sklearn.linear_model import LinearRegression
