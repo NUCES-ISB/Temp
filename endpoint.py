fe85e0cdae86a7e3c96e8b4d4fa552d1950b8cce
shazilahmed17 <124556496+shazilahmed17@users.noreply.github.com>
2023-03-24 15:16:35 +0500

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,1 +1,10 @@
 
+from flask import Flask, request, jsonify,render_template
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import pandas as pd
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+import numpy as np
+import joblib
